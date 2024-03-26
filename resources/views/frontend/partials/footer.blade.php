ccec6e7c975e3ae989b2b57a6a0cd5229280a6a3
SetiaOnce <gedeyoga1126@gmail.com>
2024-03-26 08:14:50 +0700

    Update footer.blade.php

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -3,9 +3,18 @@
     <!--begin::Footer container-->
     <div class="app-container  container-fluid d-flex flex-column flex-md-row flex-center py-3 ">
         <!--begin::Copyright-->
-        <div class="text-dark order-2 order-md-1 w-100 text-center" id="footerCopyright">
-            <div class="h3 placeholder-glow">
-                <span class="placeholder col-3"></span>
+        <div class="text-dark order-2 order-md-1 w-100 text-center" id="footer">
+            <div class="row justify-content-between">
+                <div class="col-md-6 visitors">
+                    <div class="h3 placeholder-glow rounded">
+                        <span class="placeholder col-3 rounded"></span>
+                    </div>
+                </div>
+                <div class="col-md-6 copyright">
+                    <div class="h3 placeholder-glow rounded">
+                        <span class="placeholder col-3 rounded"></span>
+                    </div>
+                </div>
             </div>
         </div>
         <!--end::Copyright-->
